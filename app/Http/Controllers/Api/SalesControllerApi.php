725cf917fbad79a868c73252a2fe1d07c90ce6b5
GavraAM91 <125192790+GavraAM91@users.noreply.github.com>
2025-02-01 21:49:41 +0700

    adding system to sales api

--- app/Http/Controllers/Api/SalesControllerApi.php
+++ app/Http/Controllers/Api/SalesControllerApi.php
@@ -9,6 +9,7 @@ use App\Models\Membership;
 use Illuminate\Http\Request;
 use App\Models\MembershipBenefits;
 use App\Http\Controllers\Controller;
+use App\Models\selling_price;
 use Illuminate\Support\Facades\Validator;
 
 
@@ -26,7 +27,7 @@ class SalesControllerApi extends Controller
             'cart' => 'required|array',
             'cart.*.membership_id' => 'nullable',
             'cart.*.product_id' => 'required',
-            'cart.*.coupon_name' => 'nullable',
+            'cart.*.coupon_code' => 'nullable',
             'cart.*.quantity' => 'required|integer|min:1',
             'cart.*.total_price' => 'required',
         ]);
@@ -49,7 +50,7 @@ class SalesControllerApi extends Controller
         $totalPrice = $request->total_price;
 
         //if they use membership card
-        if ($request->membership_id != null) {
+        if ($request->membership_id) {
             //insert membership_id from request 
             $membershipId = $request->membeship_id;
 
@@ -70,14 +71,68 @@ class SalesControllerApi extends Controller
         }
 
         //purchase system
-        foreach($request->cart as $item) {
+        foreach ($request->cart as $item) {
             $product = Product::findOrFail($item['product_id']);
 
-                
+            //insert product price
+            $basePrice = $product->product_price;
+
+            //check the membership type
+            if ($membershipData) {
+                $selling_price = selling_price::where('type', $membershipData->type)->first();
+                $markup = $selling_price ? $selling_price->markup / 100 : 0.03;
+            } else {
+                $markup = 0.03;
+            }
+
+            //counting + margin
+            $price = $basePrice + ($basePrice * $markup);
+
+            //check if any discount added
+            $discountByCoupon = 0;
+
+            if (!empty($item['coupon_code'])) {
+                $coupon = Coupon::where('coupon_code', $item['coupon_code'])->first();
+                if ($coupon->used_coupon <= $coupon->total_coupon) {
+                    //if any coupon available, edit the table
+                    $coupon->used_coupon += 1;
+                    $coupon->save();
+
+                    if (!empty($coupon->percentage_coupon)) {
+                        $discountByCoupon = $coupon->percentage_coupon;
+                    } else {
+                        $discountByCoupon = $coupon->value_coupon;
+                    }
+                } else {
+                    $discountByCoupon = 0;
+                }
+            }
+
+            //perhitungann 
+            $subtotal = ($price * $item['quantity']) - $discountByCoupon;
+
+            //hitung pajak 
+            $tax = $subtotal * 0.11;
+
+            //point membership
+            $points = ($membershipData && in_array($membershipData->type, ['type1', 'type2'])) ? ($subtotal * 0.02) : 0;
+
+            //total price
+            $totalPrice += $subtotal + $tax;
+            $pointMembership += $points;
+
+            //insert into membership point 
+            Membership::where('id', $membershipData->id)->update([
+                'points' => $membershipData->poin + $pointMembership,
+            ]);
+
+            //insert into sales 
+            Sales::create([
+                'membership_id' => $item['membership_id'],
+                ''
+            ]);
+
         }
-
-        //Tax Value
-
 
 
         //save in database sales
